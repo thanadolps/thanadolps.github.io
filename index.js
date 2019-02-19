e0d265cf324a83aec7817370cfce028422f5d9c0
thanadolps <thanadolps@gmail.com>
2019-02-20 00:06:19 +0700

    Add Random Button

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 let container_slider_blocks = document.getElementById("container_slider_blocks");
 let prototype_sliders_block = document.getElementById("prototype_sliders_block")
 
-
-decorate_slider_block(prototype_sliders_block.firstElementChild, prototype_sliders_block.lastElementChild, 0);
-for(let i = 1 ; i < 25 ; i++)
+function populate_slider_block(count, variances_ratio)
 {
-    const slider_block = prototype_sliders_block.cloneNode(true);
-    decorate_slider_block(slider_block.firstElementChild, slider_block.lastElementChild, i);
+    decorate_slider_block(prototype_sliders_block.firstElementChild,
+         prototype_sliders_block.lastElementChild, 0, variances_ratio[0]);
+    for(let i = 1 ; i < count ; i++)
+    {
+        const slider_block = prototype_sliders_block.cloneNode(true);
+        decorate_slider_block(slider_block.firstElementChild, slider_block.lastElementChild, i, variances_ratio[i]);
 
-    container_slider_blocks.appendChild(slider_block);
+        container_slider_blocks.appendChild(slider_block);
+    }
 }
 
 /**
@@ -18,10 +21,11 @@ for(let i = 1 ; i < 25 ; i++)
  * @param {HTMLInputElement} slider
  * @param {Number} index 
  */
-function decorate_slider_block(label, slider, index)
+function decorate_slider_block(label, slider, index, variance_ratio)
 {
     const display_number = ("0" + (index + 1)).slice(-2);
-    label.innerHTML = `<span>PC ${display_number}: <b></span>0.00<span></span></b>`;
+    let variance_percent = (100 * variance_ratio).toPrecision(2);
+    label.innerHTML = `PC ${display_number}: <span style='float:right'>(${variance_percent}%)</span><b>0.00</b>`;
     slider.oninput = function()
     {
         label.lastChild.textContent = parseFloat(this.value).toFixed(2);
@@ -84,7 +88,7 @@ function add_loading_progress()
         let infer_button = document.getElementById('button_inference');
         
         infer_button.disabled = false;
-        infer_button.classList.replace("w3-red", "w3-green");
+        infer_button.classList.replace("w3-red", "w3-teal");
         infer_button.textContent = "Aliasing";
         infer_button.onclick = toggle_blur;
 
@@ -131,8 +135,13 @@ function fetchModel()
     jQuery.getJSON("pca.json").done((jsonArray) => {
         pca_mean = tf.tensor(jsonArray['mean'])
         pca_component = tf.tensor(jsonArray['component'])
+        let pca_variances_ratio = jsonArray['variances_ratio']
 
+        console.assert(pca_mean.size == pca_component.shape[1], "PCA Mean and Component are Not Compatible")
         console.log("PCA Loaded")
+        
+        populate_slider_block(pca_mean.size, pca_variances_ratio);
+
         add_loading_progress();
     })
 }
@@ -151,4 +160,23 @@ function inference()
     {   
         image_reconstuct.src = canvas_reconstruct.toDataURL();
     });
+}
+
+// randomization
+
+document.getElementById("button_random").onclick = function()
+{
+    const normalDistTensor = tf.randomNormal([container_slider_blocks.childElementCount], 0, 0.5);
+    const normalDist = normalDistTensor.arraySync();
+    normalDistTensor.dispose();
+    for(let i = 0 ; i < container_slider_blocks.childElementCount ; i++)
+    {
+        let slider_block = container_slider_blocks.children[i];
+        let slider = slider_block.lastElementChild;
+
+        let normal_val = normalDist[i]
+        slider.value = normal_val;
+        slider_block.firstElementChild.lastChild.textContent = normal_val.toFixed(2);
+    }
+    inference();
 }
